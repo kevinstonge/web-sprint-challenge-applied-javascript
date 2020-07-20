80e2f66124220968b97149868ab4a816f870e02b
Kevin St.Onge <kevinstonge@gmail.com>
2020-07-19 21:44:43 -0400

    cards and tabs complete

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,36 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+const createCard = (dataObject) => {
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('card');
+    const headlineDiv = document.createElement('div');
+    headlineDiv.classList.add('headline');
+    headlineDiv.innerText = dataObject.headline;
+    cardDiv.appendChild(headlineDiv);
+    const authorDiv = document.createElement('div');
+    authorDiv.classList.add('author');
+    cardDiv.appendChild(authorDiv);
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('img-container');
+    authorDiv.appendChild(imgContainer);
+    const authorPhoto = document.createElement('img');
+    authorPhoto.src = dataObject.authorPhoto;
+    imgContainer.appendChild(authorPhoto);
+    const authorSpan = document.createElement('span');
+    authorSpan.innerText = dataObject.authorName;
+    authorDiv.appendChild(authorSpan);
+
+    cardDiv.addEventListener('click',()=>{console.log(dataObject.headline)})
+    
+    return cardDiv;
+}
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles").then(r=>{
+    Object.keys(r.data.articles).forEach(cat=>{
+        r.data.articles[cat].forEach(article=>{
+            document.querySelector('.cards-container').appendChild(createCard(article))
+        })
+    })
+})
--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,12 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics").then(r=>{
+    r.data.topics.forEach(t=>{
+        tab = document.createElement('div');
+        tab.classList.add('tab');
+        tab.innerText = t;
+        document.querySelector('.topics').appendChild(tab);
+    })
+})
